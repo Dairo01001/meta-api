9c9e44b0720609f79ada0c1c4422ce085922a49a
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2024-10-03 19:24:36 -0500

    Update app.config.ts

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT || 3000;
-export const ORIGIN = process.env.ORIGIN || 'http://localhost:3000';
+export const ORIGIN = process.env.ORIGIN || 'http://localhost:5173';
