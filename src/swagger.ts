00bb4727c51535a80d6c95180915b2780cc0c5a3
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2024-12-04 11:03:04 -0500

    Doc yml

--- src/swagger.ts
+++ src/swagger.ts
@@ -1,39 +1,9 @@
 import { Application } from 'express'
-import swaggerJsdoc from 'swagger-jsdoc'
 import swaggerUi from 'swagger-ui-express'
+import yaml from 'yamljs'
 
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Meta API',
-      version: '1.0.0',
-      description: 'Uniamazonia 3D API',
-    },
-    licence: {
-      name: 'MIT',
-      url: 'https://opensource.org/licenses/MIT',
-    },
-    contact: {
-      name: 'Uniamazonia',
-      url: 'https://uniamazonia.com.br',
-      email: 'contato@uniamazonia.com.br',
-    },
-  },
-  servers: [
-    {
-      url: 'http://localhost:3001',
-    },
-  ],
-  apis: ['./src/**/*.ts'],
-}
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions)
+const swaggerDocument = yaml.load('./src/swagger.yml')
 
 export const swaggerInit = (app: Application) => {
-  app.use(
-    '/api',
-    swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, { explorer: true }),
-  )
+  app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 }
