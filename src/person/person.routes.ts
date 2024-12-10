89adf80b36dd881f057f18e6c4d06bd7b204547c
Dairo García Naranjo <50602257+Dairo01001@users.noreply.github.com>
2024-12-10 16:47:52 -0500

    update docs

--- src/person/person.routes.ts
+++ src/person/person.routes.ts
@@ -1,13 +1,23 @@
-import { Router } from 'express';
-import { deserializeUser, validateResource } from '../middlewares';
-import { CreatePersonSchema } from './person.schema';
-import { createPersonHandler, updatePersonHandler } from './person.controller';
+import { Router } from 'express'
+import { deserializeUser, validateResource } from '../middlewares'
+import { createPersonHandler, updatePersonHandler } from './person.controller'
+import { CreatePersonSchema } from './person.schema'
 
 export const personRoutes = (): Router => {
-  const router = Router();
+  const router = Router()
 
-  router.post('/', deserializeUser, validateResource(CreatePersonSchema), createPersonHandler);
-  router.put('/:id', deserializeUser, validateResource(CreatePersonSchema), updatePersonHandler);
+  router.post(
+    '/',
+    deserializeUser,
+    validateResource(CreatePersonSchema),
+    createPersonHandler,
+  )
+  router.put(
+    '/:id',
+    deserializeUser,
+    validateResource(CreatePersonSchema),
+    updatePersonHandler,
+  )
 
-  return router;
-};
+  return router
+}
--- src/profile/profile.routes.ts
+++ src/profile/profile.routes.ts
@@ -1,14 +1,33 @@
-import { Router } from 'express';
-import { deserializeUser, validateResource } from '../middlewares';
-import { CreateProfileSchema } from './profile.schema';
-import { createProfileHandler, updateProfileHandler, upsertProfileHandler } from './profile.controller';
+import { Router } from 'express'
+import { deserializeUser, validateResource } from '../middlewares'
+import {
+  createProfileHandler,
+  updateProfileHandler,
+  upsertProfileHandler,
+} from './profile.controller'
+import { CreateProfileSchema } from './profile.schema'
 
 export const profileRoutes = (): Router => {
-  const router = Router();
+  const router = Router()
 
-  router.post('/', deserializeUser, validateResource(CreateProfileSchema), createProfileHandler);
-  router.put('/:id', deserializeUser, validateResource(CreateProfileSchema), updateProfileHandler);
-  router.patch('/', deserializeUser, validateResource(CreateProfileSchema), upsertProfileHandler);
+  router.post(
+    '/',
+    deserializeUser,
+    validateResource(CreateProfileSchema),
+    createProfileHandler,
+  )
+  router.put(
+    '/:id',
+    deserializeUser,
+    validateResource(CreateProfileSchema),
+    updateProfileHandler,
+  )
+  router.patch(
+    '/',
+    deserializeUser,
+    validateResource(CreateProfileSchema),
+    upsertProfileHandler,
+  )
 
-  return router;
-};
+  return router
+}
